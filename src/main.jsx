695bc3aa896f90a8cb2ee9e728496598dfb616bd
Sazzad Hossain <sazzadhossainadib@gmail.com>
2024-11-26 02:10:01 +0600

    update main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import store from "./store/store";
 import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./store/store";
+
+console.log("main.jsx: Starting React app...");
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
+  console.log("main.jsx: Root element found, mounting React app...");
   ReactDOM.createRoot(rootElement).render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   );
+} else {
+  console.error("main.jsx: Root element not found!");
 }
